bc8a4b4d7c818471a776969b3e49c2cf9edf82c5
ASM_DANENTANG2 <minhktph40749@fpt.edu.vn>
2024-09-24 08:13:55 +0700

    24/09

--- app/src/main/java/acount/poly/minhkt_imtatech_duan/Fragment/SuaChiTietSP_Frg.java
+++ app/src/main/java/acount/poly/minhkt_imtatech_duan/Fragment/SuaChiTietSP_Frg.java
@@ -1,1 +1,26 @@
 package acount.poly.minhkt_imtatech_duan.Fragment;
+
+import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import acount.poly.minhkt_imtatech_duan.Dao.SanPham_Dao;
+import acount.poly.minhkt_imtatech_duan.Model.SanPham;
+import acount.poly.minhkt_imtatech_duan.Model.TheLoai;
+import acount.poly.minhkt_imtatech_duan.R;
+
+import java.util.ArrayList;
